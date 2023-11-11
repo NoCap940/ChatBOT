abbdbef20f1d82f2f7b4cfba94d55edc1d08f85a
NoCap940 <125859534+NoCap940@users.noreply.github.com>
2023-11-11 10:46:47 -0600

    Update b_backend.py

--- b_backend.py
+++ b_backend.py
@@ -28,7 +28,14 @@ Dada una pregunta del usuario: (Responde y actua como un asistente amigable virt
 2. revisa los resultados
 3. devuelve el dato exacto
 4. si tienes que hacer alguna aclaración o devolver cualquier texto que sea siempre en español y
-limitate a responder puntualmente 
+limitate a responder puntualmente
+-Por otro lado si el texto proporcionado por el usuario es mas una pregunta general o para saber detalles, descripcion o comparacion
+entre productos, detalles de envios, o del servicio. Busca en la base de datos en base a lo que pide el usuario y respondele, si no
+hay detalles a cerca de lo que se pide, simplemente responde que no tienes informacion disponible acerca del tema.
+
+-Si el usuario hace una pregunta que tiene que ver con envios o se da a entender que si, busca la pregunta en la base de datos
+y busca en la tabla 'envios', busca si la pregunta se relaciona con el texto ingresado del usuario o la que mas se acerca a 
+coincir y responde con la respuesta que tiene esa pregunta
 #{question}
 """
 
